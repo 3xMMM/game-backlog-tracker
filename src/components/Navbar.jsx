9b9a89ab84cafd76c9dbfe2127cf3c65914c40f0
Mark Martinez <markmmartinez333@gmail.com>
2022-01-17 15:18:27 -0800

    Added basic Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <nav className="Navbar">
+            <span className="Navbar__Left">Game Backlog Tracker</span>
+        </nav>
+    );
+}
+  
+export default Navbar;
